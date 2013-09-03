3bfa204ea72f0d201b471cc802bf966e5b1fd59c
Michael Minella <mminella@gopivotal.com>
2013-09-03 15:10:19 -0500

    Added injection examples

--- src/main/java/org/springframework/batch/jsr/demo/batchlet/HelloWorldBatchlet.java
+++ src/main/java/org/springframework/batch/jsr/demo/batchlet/HelloWorldBatchlet.java
@@ -1,13 +1,23 @@
 package org.springframework.batch.jsr.demo.batchlet;
 
+import javax.batch.api.BatchProperty;
 import javax.batch.api.Batchlet;
+import javax.inject.Inject;
 
 public class HelloWorldBatchlet implements Batchlet {
+
+	@Inject
+	@BatchProperty
+	private String greeting = "Hello";
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
 
 	@Override
 	public String process() throws Exception {
 		System.err.println("********************************************");
-		System.err.println("          Hello, SpringOne!");
+		System.err.println("          " + greeting + ", SpringOne!");
 		System.err.println("********************************************");
 		return null;
 	}

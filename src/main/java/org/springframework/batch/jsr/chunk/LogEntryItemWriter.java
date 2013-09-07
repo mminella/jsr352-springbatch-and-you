e9281f4d4fa4b019b1e58286cfccc84cfa035996
Michael Minella <mminella@gopivotal.com>
2013-09-06 19:09:20 -0500

    Completed writing portion of the job

--- src/main/java/org/springframework/batch/jsr/chunk/LogEntryItemWriter.java
+++ src/main/java/org/springframework/batch/jsr/chunk/LogEntryItemWriter.java
@@ -1,32 +1,51 @@
 package org.springframework.batch.jsr.chunk;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 import javax.batch.api.chunk.ItemWriter;
 import javax.sql.DataSource;
 
+import org.springframework.batch.jsr.domain.LogEntry;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class LogEntryItemWriter implements ItemWriter {
 
 	@Autowired
 	public DataSource dataSource;
+	private Connection connection;
+	private PreparedStatement statement;
 
 	@Override
 	public void open(Serializable checkpoint) throws Exception {
+		connection = dataSource.getConnection();
+		statement = connection.prepareStatement("INSERT INTO logEntry (ip_address, requested_url, country_code, view_date) VALUES(?, ?, ?, ?)");
 	}
 
 	@Override
 	public void close() throws Exception {
+		statement.close();
+		connection.close();
 	}
 
 	@Override
 	public void writeItems(List<Object> items) throws Exception {
 		if(items != null && !items.isEmpty()) {
-			for (Object entry : items) {
+			for (Object item : items) {
+				LogEntry entry = (LogEntry) item;
 
+				statement.setString(1, entry.getIpAddress());
+				statement.setString(2, entry.getRequestedUrl());
+				statement.setString(3, entry.getCountryCode());
+				statement.setDate(4, new Date(entry.getViewDate().getTime()));
+
+				statement.addBatch();
 			}
+
+			statement.executeBatch();
 		}
 	}
 
--- src/main/resources/support/logEntry-mysql.sql
+++ src/main/resources/support/logEntry-mysql.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS logEntry;
 CREATE TABLE logEntry  (
 	id BIGINT NOT NULL AUTO_INCREMENT,
 	ip_address CHAR(15),
-	requested_url VARCHAR(255),
+	requested_url TEXT,
 	country_code CHAR(2),
 	view_date DATE,
 	PRIMARY KEY (id)

f8bdbcd69135d1a3cf764c10678c10a5cacb6a69
Michael Minella <mminella@gopivotal.com>
2013-09-06 21:23:13 -0500

    Fixed restart functionality in JsrMainClass

--- src/main/java/org/springframework/batch/jsr/JsrMainClass.java
+++ src/main/java/org/springframework/batch/jsr/JsrMainClass.java
@@ -14,16 +14,21 @@ public class JsrMainClass {
 		JobOperator jobOperator = BatchRuntime.getJobOperator();
 
 		Properties jobParameters = new Properties();
-		boolean restart = false;
+
+		long executionId = -1;
+
+		try {
+			executionId = Long.parseLong(args[0]);
+		} catch (NumberFormatException notRestart) {
+		}
 
 		for(int i = 1; i < args.length; i++) {
 			jobParameters.put(args[i].substring(0, args[i].indexOf('=')), args[i].substring(args[i].indexOf('=') + 1));
 		}
 
-		long executionId = -1;
 
-		if(restart) {
-			executionId = jobOperator.restart(Long.parseLong(args[0]), jobParameters);
+		if(executionId >= 0) {
+			executionId = jobOperator.restart(executionId, jobParameters);
 		} else {
 			executionId = jobOperator.start(args[0], jobParameters);
 		}
